010e04f849291ea4e83191e8f72790895746b2ea
unknown <mothcar@gmail.com>
2016-02-09 05:24:44 +0900

    icon

--- app/src/main/java/com/umanji/umanjiapp/ui/page/main/MainFragment.java
+++ app/src/main/java/com/umanji/umanjiapp/ui/page/main/MainFragment.java
@@ -207,8 +207,9 @@ public class MainFragment extends BaseFragment {
                                 mMarker = mMap.addMarker(new MarkerOptions().position(mPoint)
                                         .title(mCurrentChannel.getName())
                                         .snippet(FOCUSED_ITEM_MARKER)
-                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker_aqua))
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.kr))
                                         .anchor(0.45f, 1.0f));
+                                mMarker.setAlpha(127);
                                 break;
 
                         }
@@ -516,7 +517,7 @@ public class MainFragment extends BaseFragment {
                             default:
                                 mMarker = mMap.addMarker(new MarkerOptions().position(mPoint)
                                         .title("스팟생성")
-                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker_aqua))
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.kr))
                                         .anchor(0.45f, 1.0f));
                                 break;
                         }
