6e30141b237f2b978de8cc63593588eba1575605
unknown <mothcar@gmail.com>
2016-03-03 17:57:33 +0900

    url extract 함수 FileHelper로 뺌

--- app/src/main/java/com/umanji/umanjiapp/ui/channel/_fragment/BaseChannelListAdapter.java
+++ app/src/main/java/com/umanji/umanjiapp/ui/channel/_fragment/BaseChannelListAdapter.java
@@ -45,10 +45,10 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.TimeZone;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import de.greenrobot.event.EventBus;
+
+import static com.umanji.umanjiapp.helper.FileHelper.extractUrls;
 
 
 public abstract class BaseChannelListAdapter extends RecyclerView.Adapter<BaseChannelListAdapter.ViewHolder> implements AppConfig {
@@ -400,24 +400,7 @@ public abstract class BaseChannelListAdapter extends RecyclerView.Adapter<BaseCh
         }
     }
 
-    /**
-     * Returns a list with all links contained in the input
-     */
-    public static List<String> extractUrls(String text)
-    {
-        List<String> containedUrls = new ArrayList<String>();
-        String urlRegex = "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
-        Pattern pattern = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE);
-        Matcher urlMatcher = pattern.matcher(text);
 
-        while (urlMatcher.find())
-        {
-            containedUrls.add(text.substring(urlMatcher.start(0),
-                    urlMatcher.end(0)));
-        }
-
-        return containedUrls;
-    }
 
     protected void setPhoto(final ViewHolder holder, final ChannelData channelData) {
         String photo = channelData.getPhoto();
