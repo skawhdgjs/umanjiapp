6ab1eb950de62b5231eaba11238d4eb7ae972c2e
unamedkr <unamed.kr@gmail.com>
2016-03-16 00:55:32 +0900

    키워드 수정

--- app/src/main/java/com/umanji/umanjiapp/ui/channel/_fragment/about/AboutFragment.java
+++ app/src/main/java/com/umanji/umanjiapp/ui/channel/_fragment/about/AboutFragment.java
@@ -5,12 +5,14 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androidquery.callback.AjaxCallback;
 import com.androidquery.callback.AjaxStatus;
@@ -192,6 +194,36 @@ public class AboutFragment extends BaseChannelListFragment {
     @Override
     public void onEvent(SuccessData event) {
         super.onEvent(event);
+
+        ChannelData channelData = new ChannelData(event.response);
+
+        switch (event.type) {
+            case api_channels_create:
+                String parentId = event.response.optString("parent");
+                if(TextUtils.equals(mChannel.getId(), parentId)) {
+                    try {
+                        JSONObject params = new JSONObject();
+                        params.put("id", mChannel.getId());
+                        mApi.call(api_channels_get, params, new AjaxCallback<JSONObject>() {
+                            @Override
+                            public void callback(String url, JSONObject object, AjaxStatus status) {
+                                mChannel = new ChannelData(object);
+                            }
+                        });
+                        updateView();
+                    } catch(JSONException e) {
+                        Log.e(TAG, "error " + e.toString());
+                    }
+
+                    if(TextUtils.isEmpty(channelData.getId())) {
+                        Toast.makeText(mActivity, "이미 존재하는 키워드 입니다.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        mAdapter.addTop(channelData);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }
+                break;
+        }
     }
 
     @Override
