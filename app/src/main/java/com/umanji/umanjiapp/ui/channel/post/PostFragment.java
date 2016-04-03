22547a44f125bdb306be573a12750ed322413557
unknown <mothcar@gmail.com>
2016-04-03 15:46:16 +0900

    new post UI

--- app/src/main/java/com/umanji/umanjiapp/ui/channel/post/PostFragment.java
+++ app/src/main/java/com/umanji/umanjiapp/ui/channel/post/PostFragment.java
@@ -75,7 +75,7 @@ public class PostFragment extends BaseChannelFragment {
         super.updateView();
 
         setName(mActivity, mChannel, "내용없음");
-        setParentName(mActivity, mChannel.getParent());
+        //setParentName(mActivity, mChannel.getParent());
         setPhoto(mActivity, mChannel, R.drawable.reply_background);
     }
 
