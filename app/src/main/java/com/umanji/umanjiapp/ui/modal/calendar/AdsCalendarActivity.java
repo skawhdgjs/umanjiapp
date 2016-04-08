579b60774e952f072eee0251a481df351a43c72a
unknown <mothcar@gmail.com>
2016-04-09 04:10:32 +0900

    광고날짜

--- app/src/main/java/com/umanji/umanjiapp/ui/modal/calendar/AdsCalendarActivity.java
+++ app/src/main/java/com/umanji/umanjiapp/ui/modal/calendar/AdsCalendarActivity.java
@@ -1,5 +1,6 @@
 package com.umanji.umanjiapp.ui.modal.calendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CalendarView;
@@ -25,7 +26,6 @@ public class AdsCalendarActivity extends AppCompatActivity {
 
     public void showCalendar() {
 
-
         mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
             @Override
@@ -34,10 +34,18 @@ public class AdsCalendarActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
 
                 Toast.makeText(getBaseContext(), "Selected Date is\n\n"
-                                + dayOfMonth + " : " + month + " : " + year,
+                                + year + " : " + month + " : " +  dayOfMonth,
                         Toast.LENGTH_LONG).show();
+
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("result", year + " " + month +" "+  dayOfMonth );
+                setResult(RESULT_OK,returnIntent);
+                finish();
 
             }
         });
     }
+
+
+
 }
