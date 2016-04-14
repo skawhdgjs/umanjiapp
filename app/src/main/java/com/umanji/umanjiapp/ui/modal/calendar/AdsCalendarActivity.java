54846054b4a7c2d9576267ae452071803fb96d06
unknown <mothcar@gmail.com>
2016-04-15 08:43:57 +0900

    날짜 계산 다시시작

--- app/src/main/java/com/umanji/umanjiapp/ui/modal/calendar/AdsCalendarActivity.java
+++ app/src/main/java/com/umanji/umanjiapp/ui/modal/calendar/AdsCalendarActivity.java
@@ -3,14 +3,21 @@ package com.umanji.umanjiapp.ui.modal.calendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
 import com.umanji.umanjiapp.R;
 
+import java.util.Calendar;
+
 public class AdsCalendarActivity extends AppCompatActivity {
 
     protected CalendarView mCalendarView;
+    protected int mYear;
+    protected int mMonth;
+    protected int mDay;
+    protected Calendar c = Calendar.getInstance();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,25 @@ public class AdsCalendarActivity extends AppCompatActivity {
         mCalendarView       = (CalendarView) findViewById(R.id.calendarView);
         mCalendarView.setShowWeekNumber(false);
 
-        showCalendar();
+        mYear  = c.get(Calendar.YEAR);
+        mMonth  = c.get(Calendar.MONTH) + 1;
+        mDay  = c.get(Calendar.DAY_OF_MONTH);
+
+        mCalendarView.setOnClickListener(new CalendarView.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Selected Date is\n\n" + mYear + " : " + mMonth + " : " +  mDay, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("result", mYear + " " + mMonth +" "+  mDay );
+        setResult(RESULT_OK,returnIntent);
+        finish();
+
+
+        //showCalendar();
 
     }
 
@@ -31,17 +56,10 @@ public class AdsCalendarActivity extends AppCompatActivity {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month,
                                             int dayOfMonth) {
-                // TODO Auto-generated method stub
 
-                Toast.makeText(getBaseContext(), "Selected Date is\n\n"
+               /* Toast.makeText(getBaseContext(), "Selected Date is\n\n"
                                 + year + " : " + month + " : " +  dayOfMonth,
-                        Toast.LENGTH_LONG).show();
-
-                Intent returnIntent = new Intent();
-                returnIntent.putExtra("result", year + " " + month +" "+  dayOfMonth );
-                setResult(RESULT_OK,returnIntent);
-                finish();
-
+                        Toast.LENGTH_LONG).show();*/
             }
         });
     }
