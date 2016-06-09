1d3f4ff913a422c20497a5b0ff0e58a3969a5fc5
Paul Hwang <mothcar@gmail.com>
2016-06-09 20:46:54 +0900

    level 13 when community created :: again

--- app/src/main/java/com/umanji/umanjiapp/ui/mainHome/localCommunity/StepTwoFragment.java
+++ app/src/main/java/com/umanji/umanjiapp/ui/mainHome/localCommunity/StepTwoFragment.java
@@ -171,7 +171,7 @@ public class StepTwoFragment extends BaseChannelCreateFragment {
             JSONObject params = mChannel.getAddressJSONObject();
             params.put("parent", mChannel.getId());
 //            params.put("parentType", mChannel.getParent().getType());
-            params.put("level", mChannel.getLevel());
+            params.put("level", 13);
             params.put("name", mName.getText().toString());
             params.put("type", TYPE_COMMUNITY);
 
